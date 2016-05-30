66e7715bc24771a9f2f9d780a28f7b263e00c1a5
Tom McLean <tom.a.mclean@gmail.com>
2016-05-30 15:37:46 +1000

    :rotating_light:

---
+++ README.md
@@ -0,0 +1,54 @@
+# Panic Button
+
+## Usage
+
+Simply include the `panic_button.js` script anywhere in your code.
+
+```html
+<script src="https://domain-goes-here.com/panic.min.js"></script>
+```
+
+To manually define where the panic button will attach, simply create an element
+with `id="panic"`. Otherwise, the panic button will attach itself as the first
+child of `<body>`.
+
+## Styling
+
+Include `panic_style.css` or manually define styling for the following
+heirarchy:
+
+```html
+<div class="panic_container" id="panic">
+    <span class="panic_text">{ panic_config.text }</span>
+    <a class="panic_link">{ panic_config.link_text }</a>
+    <a class="panic_button">{ panic_config.button_text }</a>
+</div>
+```
+
+## Configuration 
+
+Define an object in global scope called `panic_config`, and override any
+or all of the following values. This object must be defined *before* the
+inclusion of `panic_button.js`.
+
+```js
+panic_config = {
+    // the site that the panic button will redirect to when triggered
+    escape_site: 'http://www.google.com/',
+
+    // the text in the banner
+    text: 'Protect yourself online.',
+
+    // the link in the banner -- text and target
+    link_site: 'http://help',
+    link_text: 'Learn how to hide your browsing history.',
+
+    // the text on the panic button itself (&times; used for 'x' button)
+    button_text: '&times; Quick exit (ESC)',
+
+    // define which key will trigger the panic button (or null to disable)
+    // (don't forget to alter button_text if you change this)
+    panic_hotkey: 27    // escape
+};
+```
+
---
+++ panic.js
@@ -0,0 +1,115 @@
+
+(function() {
+
+    //---------------------------------------------------------
+    // Object.assign polyfill
+    // from https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
+
+    if (typeof Object.assign != 'function') {
+        Object.assign = function(target) {
+            'use strict';
+            if (target == null) {
+                throw new TypeError('Cannot convert undefined or null to object');
+            }
+
+            target = Object(target);
+            for (var index = 1; index < arguments.length; index++) {
+                var source = arguments[index];
+                if (source != null) {
+                    for (var key in source) {
+                        if (Object.prototype.hasOwnProperty.call(source, key)) {
+                            target[key] = source[key];
+                        }
+                    }
+                }
+            }
+            return target;
+        };
+    }
+    //--------------------------------------------------------- 
+
+    // get/update the config object
+    if(typeof panic_config === 'undefined') {
+        panic_config = {};
+    }
+
+    var config = Object.assign({
+        escape_site: 'http://www.google.com/',
+        text: 'Protect yourself online.',
+        link_site: 'https://www.domesticshelters.org/safe-surfing',
+        link_text: 'Learn how to hide your browsing history.',
+        button_text: '&times; Quick exit (ESC)',
+        hotkey: 27
+    }, panic_config);
+
+    // panic implementation
+    window.panic = function() {
+        // wipe the whole DOM first in case of slow internet connection
+        var html = document.getElementsByTagName('html')[0];
+        while(html.firstChild) { html.removeChild(html.firstChild); }
+
+        // then redirect to a non-sensitive site
+        window.location.href = config.escape_site;
+
+        // disable default event handling
+        return false;
+    };
+
+    // add hotkey trigger
+    if(config.hotkey) {
+        document.addEventListener('keydown', function(e) {
+            if(e.keyCode === config.hotkey) {
+                panic();
+            }
+        });
+    }
+
+    function initPanic() {
+        // get root element
+        var root = document.getElementById('panic');
+        if(!root) {
+            root = document.createElement('div');
+            root.setAttribute('class', 'panic_container');
+            root.setAttribute('id', 'panic');
+
+            var body = document.body;
+            body.insertBefore(root, body.firstChild);
+        }
+
+        // add "Protect yourself online" text
+        if(config.text) { 
+            var text = document.createElement('span');
+            text.innerHTML = config.text;
+            text.setAttribute('class', 'panic_text');
+            root.appendChild(text);
+            root.appendChild(document.createTextNode(' '));
+        }
+
+        // add link to more information
+        if(config.link_site) {
+            var link = document.createElement('a');
+            link.innerHTML = config.link_text;
+            link.setAttribute('href', config.link_site);
+            link.setAttribute('class', 'panic_link');
+            root.appendChild(link);
+            root.appendChild(document.createTextNode(' '));
+        }
+
+        // add actual panic button
+        var button = document.createElement('a');
+        button.setAttribute('href', '#');
+        button.setAttribute('class', 'panic_button');
+        button.innerHTML = config.button_text;
+        button.onclick = panic;
+        root.appendChild(button);
+
+    }
+
+    if(document.readyState !== 'loading') {
+        // script was included asynchronously, call init immediately
+        initPanic();
+    } else {
+        document.addEventListener('DOMContentLoaded', initPanic);
+    }
+
+})();
