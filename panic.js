6feac9dddf47425a62c70de2598306b1e3976d16
Nathan Thorpe <nath.thorpe@gmail.com>
2016-06-03 15:17:32 +1000

    Adding default styles, moving icon into element

--- panic.js
+++ panic.js
@@ -1,4 +1,3 @@
-
 (function() {
 
     //---------------------------------------------------------
@@ -26,7 +25,7 @@
             return target;
         };
     }
-    //--------------------------------------------------------- 
+    //---------------------------------------------------------
 
     // get/update the config object
     if(typeof panic_config === 'undefined') {
@@ -38,7 +37,8 @@
         text: 'Protect yourself online.',
         link_site: 'https://www.domesticshelters.org/safe-surfing',
         link_text: 'Learn how to hide your browsing history.',
-        button_text: '&times; Quick exit (ESC)',
+        button_icon: '&times;',
+        button_text: 'Quick exit (ESC)',
         hotkey: 27
     }, panic_config);
 
@@ -69,7 +69,7 @@
         var root = document.getElementById('panic');
         if(!root) {
             root = document.createElement('div');
-            root.setAttribute('class', 'panic_container');
+            root.setAttribute('class', 'panic-button');
             root.setAttribute('id', 'panic');
 
             var body = document.body;
@@ -77,31 +77,42 @@
         }
 
         // add "Protect yourself online" text
-        if(config.text) { 
+        if(config.text) {
             var text = document.createElement('span');
             text.innerHTML = config.text;
-            text.setAttribute('class', 'panic_text');
+            text.setAttribute('class', 'panic-button__text');
             root.appendChild(text);
             root.appendChild(document.createTextNode(' '));
         }
+
 
         // add link to more information
         if(config.link_site) {
             var link = document.createElement('a');
             link.innerHTML = config.link_text;
             link.setAttribute('href', config.link_site);
-            link.setAttribute('class', 'panic_link');
+            link.setAttribute('class', 'panic-button__link');
             root.appendChild(link);
             root.appendChild(document.createTextNode(' '));
         }
 
+
         // add actual panic button
         var button = document.createElement('a');
+        var buttonText = document.createTextNode(config.button_text);
         button.setAttribute('href', '#');
-        button.setAttribute('class', 'panic_button');
-        button.innerHTML = config.button_text;
+        button.setAttribute('class', 'panic-button__button');
+        button.appendChild(buttonText);
         button.onclick = panic;
         root.appendChild(button);
+
+        // add icon to button
+        if(config.button_icon) {
+            var icon = document.createElement('span');
+            icon.innerHTML = config.button_icon;
+            icon.setAttribute('class', 'panic-button__icon');
+            button.insertBefore(icon, buttonText);
+        }
 
     }
 
