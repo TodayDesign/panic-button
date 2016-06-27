9a070cae0fc24c176f87aee927c4e6eaaabdbb52
Tom McLean <tom.a.mclean@gmail.com>
2016-06-27 17:35:49 +1000

    Add a script to update the readme with correct CDN links

---
+++ update-readme.js
@@ -0,0 +1,30 @@
+var fs = require('fs');
+
+// get current version
+var data = require('./package.json');
+var version = 'v' + data.version;
+
+// determine CDN urls
+var rawgit = 'https://cdn.rawgit.com/studiothick/panic-button/' + version + '/';
+var panic = rawgit + 'panic.min.js';
+var style = rawgit + 'panic.css';
+
+// place urls into snippet
+var snippet = ('' + fs.readFileSync('sources/snippet.html'))
+        .trim()
+        .replace('%%PANIC_MIN_JS%%', panic)
+        .replace('%%PANIC_CSS%%', style)
+        ;
+
+// place snippet into readme
+var readme = '' + fs.readFileSync('readme.md');
+var comment_re = /<!--[\w ]+-->/g;
+var chunks = readme.split(comment_re);
+
+if(chunks.length === 3) {
+    chunks[1] = '' + snippet;
+} else {
+    throw Error("The update readme script currently can't support multiple HTML blocks in the readme.");
+}
+
+fs.writeFileSync('readme.md', chunks.join(''));
