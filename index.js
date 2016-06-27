06d61f803b1f5d8bcdc4d362d6daad7bedaf68e9
Tom McLean <tom.a.mclean@gmail.com>
2016-06-27 17:00:51 +1000

    Switch to pasted-HTML instead of pure JS

--- index.js
+++ index.js
@@ -1,69 +1,37 @@
 (function() {
 
-    //---------------------------------------------------------
-    // Object.assign polyfill
-    // from https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
-
-    if (typeof Object.assign != 'function') {
-        Object.assign = function(target) {
-            'use strict';
-            if (target == null) {
-                throw new TypeError('Cannot convert undefined or null to object');
-            }
-
-            target = Object(target);
-            for (var index = 1; index < arguments.length; index++) {
-                var source = arguments[index];
-                if (source != null) {
-                    for (var key in source) {
-                        if (Object.prototype.hasOwnProperty.call(source, key)) {
-                            target[key] = source[key];
-                        }
-                    }
-                }
-            }
-            return target;
-        };
-    }
-    //---------------------------------------------------------
-
-    // get/update the config object
-    if(typeof panic_config === 'undefined') {
-        panic_config = {};
-    }
-
-    var config = Object.assign({
-        escape_site: 'http://www.google.com/',
-        text: 'Protect yourself online.',
-        link_site: 'https://www.domesticshelters.org/safe-surfing',
-        link_text: 'Learn how to hide your browsing history.',
-        button_icon: '&times;',
-        button_text: 'Quick exit (ESC)',
-        hotkey: 27
-    }, panic_config);
-
-    var redirect = function() {
-        window.location.href = config.escape_site;
-    };
-
     // panic implementation
-    window.panic = function() {
+    window.panic = function(site, e) {
         // wipe the whole DOM first in case of slow internet connection
-        var html = document.getElementsByTagName('html')[0];
-        while(html.firstChild) { html.removeChild(html.firstChild); }
+        var body = document.getElementsByTagName('body')[0];
+        while(body.firstChild) { body.removeChild(body.firstChild); }
+
+        // remove as much info from URL as possible
+        if(window.history) {
+            window.history.replaceState({}, '', '/');
+        }
 
         // then redirect to a non-sensitive site
-        redirect();
+        window.location.href = site;
 
         // disable default event handling
         return false;
     };
 
-    // add hotkey trigger
-    if(config.hotkey) {
+    function initPanic() {
+        
+        var button = document.getElementById('panic-button');
+        var escape_site = button.getAttribute('href');
+
+        // add click handler
+        button.onclick = function(e) {
+            return panic(escape_site);
+        };
+
+        // add hotkey trigger
         document.addEventListener('keydown', function(e) {
             if(e.keyCode === config.hotkey) {
-                panic();
+                panic(escape_site);
 
                 if(e) {
                     // stop escape from cancelling redirect
@@ -76,65 +44,9 @@
                 return false;
             }
         });
-    }
-
-    function initPanic() {
-        // get root element
-        var root = document.getElementById('panic');
-        if(!root) {
-            root = document.createElement('div');
-            root.setAttribute('class', 'panic-button');
-            root.setAttribute('id', 'panic');
-
-            var body = document.body;
-            body.insertBefore(root, body.firstChild);
-        }
-
-        // add "Protect yourself online" text
-        if(config.text) {
-            var text = document.createElement('span');
-            text.innerHTML = config.text;
-            text.setAttribute('class', 'panic-button__text');
-            root.appendChild(text);
-            root.appendChild(document.createTextNode(' '));
-        }
-
-
-        // add link to more information
-        if(config.link_site) {
-            var link = document.createElement('a');
-            link.innerHTML = config.link_text;
-            link.setAttribute('href', config.link_site);
-            link.setAttribute('class', 'panic-button__link');
-            root.appendChild(link);
-            root.appendChild(document.createTextNode(' '));
-        }
-
-
-        // add actual panic button
-        var button = document.createElement('a');
-        var buttonText = document.createTextNode(config.button_text);
-        button.setAttribute('href', '#');
-        button.setAttribute('class', 'panic-button__button');
-        button.appendChild(buttonText);
-        button.onclick = panic;
-        root.appendChild(button);
-
-        // add icon to button
-        if(config.button_icon) {
-            var icon = document.createElement('span');
-            icon.innerHTML = config.button_icon;
-            icon.setAttribute('class', 'panic-button__icon');
-            button.insertBefore(icon, buttonText);
-        }
 
     }
 
-    if(document.readyState !== 'loading') {
-        // script was included asynchronously, call init immediately
-        initPanic();
-    } else {
-        document.addEventListener('DOMContentLoaded', initPanic);
-    }
+    initPanic();
 
 })();
