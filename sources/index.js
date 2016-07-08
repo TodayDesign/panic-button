a5fc8d68967f95e0e4e0584f86e4648f050d1e5a
Tom McLean <tom.a.mclean@gmail.com>
2016-07-08 14:11:37 +1000

    Immediately switch title instead of using filename of page

--- sources/index.js
+++ sources/index.js
@@ -1,7 +1,7 @@
 (function() {
 
     // panic implementation
-    window.panic = function(site, e) {
+    window.panic = function(site, title) {
         // wipe the whole DOM first in case of slow internet connection
         var html = document.getElementsByTagName('html')[0];
         while(html.firstChild) { html.removeChild(html.firstChild); }
@@ -11,14 +11,16 @@
         // separate step
         html.removeAttribute('style');
 
+        // replace the title
+        html.innerHTML = '<head><title>' + title + '</title></head>';
+
+        // then redirect to a non-sensitive site
+        window.location.href = site;
 
         // remove as much info from URL as possible
         if(window.history) {
             window.history.replaceState({}, '', '/');
         }
-
-        // then redirect to a non-sensitive site
-        window.location.href = site;
 
         // disable default event handling
         return false;
@@ -28,16 +30,18 @@
         
         var button = document.getElementById('panic-button');
         var escape_site = button.getAttribute('href');
+        var escape_site_title = button.getAttribute('title') || 'Google';
+        var hotkey = 27;
 
         // add click handler
         button.onclick = function(e) {
-            return panic(escape_site);
+            return panic(escape_site, escape_site_title);
         };
 
         // add hotkey trigger
         document.addEventListener('keydown', function(e) {
-            if(e.keyCode === config.hotkey) {
-                panic(escape_site);
+            if(e.keyCode === hotkey) {
+                panic(escape_site, escape_site_title);
 
                 if(e) {
                     // stop escape from cancelling redirect
